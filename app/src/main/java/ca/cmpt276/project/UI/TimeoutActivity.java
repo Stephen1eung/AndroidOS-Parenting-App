fefdb6b39425be4278c8c43a54cf3408cf08c48c
kja54 <kja54@sfu.ca>
2021-11-01 12:34:01 -0700

    a clean working TimeoutActivity

--- app/src/main/java/ca/cmpt276/project/UI/TimeoutActivity.java
+++ app/src/main/java/ca/cmpt276/project/UI/TimeoutActivity.java
@@ -2,22 +2,28 @@ package ca.cmpt276.project.UI;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 import ca.cmpt276.project.R;
 
+// https://www.youtube.com/watch?v=MDuGwI6P-X8&list=PLrnPJCHvNZuB8wxqXCwKw2_NkyEmFwcSd&index=1&ab_channel=CodinginFlow
+// a lot of the code was from that youtube video above, I simply added shared pref and user input (the video was missing those)
 public class TimeoutActivity extends AppCompatActivity {
+    private long START_TIME = 60000;
     private TextView countDown;
     private EditText userInput;
     private boolean timerRunning;
-    private final long START_TIME = 60000;
     private long TIME_LEFT, END_TIME;
     private CountDownTimer countDownTimer;
     private Button startAndPauseBtn, resetBtn, setBtn;
@@ -45,27 +51,139 @@ public class TimeoutActivity extends AppCompatActivity {
         startAndPauseBtn = findViewById(R.id.startAndPauseBtn);
     }
 
+    private void setTimer(long time) {
+        START_TIME = time;
+        resetTimer();
+    }
+
     private void setupTimer() {
-        startAndPauseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
+        startAndPauseBtn.setOnClickListener(view -> {
+            if (timerRunning) pauseTimer();
+            else startTimer();
         });
 
-        resetBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        resetBtn.setOnClickListener(view -> resetTimer());
 
-            }
-        });
-
-        setBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
+        setBtn.setOnClickListener(view -> {
+            if (!userInput.getText().toString().isEmpty()) {
+                if (Long.parseLong(userInput.getText().toString()) == 0) {
+                    Toast.makeText(TimeoutActivity.this, "Enter Positive Number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                long time = Long.parseLong(userInput.getText().toString()) * 60000;
+                setTimer(time);
+                userInput.setText("");
             }
         });
     }
-}
+
+    private void startTimer() {
+        END_TIME = System.currentTimeMillis() + TIME_LEFT;
+        countDownTimer = new CountDownTimer(TIME_LEFT, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                TIME_LEFT = millisUntilFinished;
+                updateCounter();
+            }
+
+            @Override
+            public void onFinish() {
+                timerRunning = false;
+                updateBtnStates();
+            }
+        }.start();
+        timerRunning = true;
+        updateBtnStates();
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        timerRunning = false;
+        updateBtnStates();
+    }
+
+    private void resetTimer() {
+        TIME_LEFT = START_TIME;
+        updateCounter();
+        updateBtnStates();
+    }
+
+    private void updateCounter() {
+        int hours = (int) (TIME_LEFT / 1000) / 3600;
+        int minutes = (int) ((TIME_LEFT / 1000) % 3600) / 60;
+        int seconds = (int) (TIME_LEFT / 1000) % 60;
+
+        String timeLeftFormatted;
+        if (hours > 0) timeLeftFormatted = String.format(Locale.getDefault(),
+                "%d:%02d:%02d", hours, minutes, seconds);
+        else timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        countDown.setText(timeLeftFormatted);
+    }
+
+    private void updateBtnStates() {
+        if (timerRunning) {
+            userInput.setVisibility(View.INVISIBLE);
+            setBtn.setVisibility(View.INVISIBLE);
+            resetBtn.setVisibility(View.INVISIBLE);
+            startAndPauseBtn.setText("Pause");
+        } else {
+            userInput.setVisibility(View.VISIBLE);
+            setBtn.setVisibility(View.VISIBLE);
+            startAndPauseBtn.setText("Start");
+
+            if (TIME_LEFT < 1000) {
+                startAndPauseBtn.setVisibility(View.INVISIBLE);
+            } else {
+                startAndPauseBtn.setVisibility(View.VISIBLE);
+            }
+
+            if (TIME_LEFT < START_TIME) {
+                resetBtn.setVisibility(View.VISIBLE);
+            } else {
+                resetBtn.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        SharedPreferences sp = getSharedPreferences("timerOutPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putLong("START_TIME", START_TIME);
+        editor.putLong("TIME_LEFT", TIME_LEFT);
+        editor.putBoolean("timerRunning", timerRunning);
+        editor.putLong("END_TIME", END_TIME);
+        editor.apply();
+
+        if (countDownTimer != null) countDownTimer.cancel();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getSharedPreferences("timerOutPref", MODE_PRIVATE);
+
+        START_TIME = prefs.getLong("START_TIME", 60000);
+        TIME_LEFT = prefs.getLong("TIME_LEFT", START_TIME);
+        timerRunning = prefs.getBoolean("timerRunning", false);
+
+        updateCounter();
+        updateBtnStates();
+
+        if (timerRunning) {
+            END_TIME = prefs.getLong("END_TIME", 0);
+            TIME_LEFT = END_TIME - System.currentTimeMillis();
+
+            if (TIME_LEFT < 0) {
+                TIME_LEFT = 0;
+                timerRunning = false;
+                updateCounter();
+                updateBtnStates();
+            } else startTimer();
+        }
+    }
 }
